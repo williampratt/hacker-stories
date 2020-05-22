af26f3f44b75c1e6791bb34ac6f669ce2ea64170
William Pratt <wpratt@Williams-MacBook-Pro-16.local>
2020-05-22 11:51:14 -0500

    revert to less concise syntax for continuing with the book

--- src/App.js
+++ src/App.js
@@ -52,18 +52,17 @@ const Search = ({search, onSearch}) => (
   </div>
 );
 
-const List = ({list}) => 
-  list.map(( {objectID, ...item }) =>
-    <Item key={objectID} {...item} /> );
+const List = ({ list }) =>
+  list.map(item => <Item key={item.objectID} item={item} />);
 
-const Item = ({ title, url, author, num_comments, points }) => (
+const Item = ({ item }) => (
   <div>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </div>
 );
 
