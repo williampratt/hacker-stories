4b2f5b5380992223ab7e64273a5f81893de80934
William Pratt <wpratt@Williams-MacBook-Pro-16.local>
2020-05-22 11:22:01 -0500

    refactored using spread and rest

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} />
+      <Search onSearch={handleSearch} search={searchTerm} />
 
       <hr />
 
@@ -45,26 +45,26 @@ const App = () => {
 
 };
 
-const Search = props => (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch}/>
-    </div>
-
+const Search = ({search, onSearch}) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" value={search} onChange={onSearch}/>
+  </div>
 );
 
-const List = props => 
-  props.list.map( item => (
-                    <div key={item.objectID}>
-                      <span>
-                        <a href={item.url}>{item.title}</a>
-                      </span>
-                      <span>{item.author}</span>
-                      <span>{item.num_comments}</span>
-                      <span>{item.points}</span>
-                    </div>
-                  )
+const List = ({list}) => 
+  list.map(( {objectID, ...item }) =>
+    <Item key={objectID} {...item} /> );
 
+const Item = ({ title, url, author, num_comments, points }) => (
+  <div>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </div>
 );
 
 export default App;
